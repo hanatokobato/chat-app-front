2871ff23bcd18ef09b66b368169dcc5cbe73e223
bangld <kobato2096@gmail.com>
2023-03-03 16:06:01 +0700

    Ws status

--- src/components/ChatItem.tsx
+++ src/components/ChatItem.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./ChatItem.scss";
 
-const ChatItem = () => {
+interface Props {
+  userName: string;
+  timestamp: string;
+  text: string;
+}
+
+const ChatItem = ({ userName, timestamp, text }: Props) => {
   return (
     <div className="message">
-      <div className="message-item user-name">MTD</div>
-      <div className="message-item timestamp">| 11:21:32:</div>
-      <div className="message-item text-message">Hello how are you</div>
+      <div className="message-item user-name">{userName}</div>
+      <div className="message-item timestamp">| {timestamp}</div>
+      <div className="message-item text-message">{text}</div>
     </div>
   );
 };
--- src/components/ChatLaylout.tsx
+++ src/components/ChatLaylout.tsx
@@ -1,29 +1,50 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./ChatLayout.scss";
 import ChatItem from "./ChatItem";
-import useWebSocket from "react-use-websocket";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 
 const socketUrl = "ws://localhost:3000/chat";
 
 const ChatLayout = () => {
   const [messageInput, setMessageInput] = useState<string>("");
-  const { sendMessage } = useWebSocket(socketUrl);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+  const [messageHistory, setMessageHistory] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      console.log(lastMessage);
+      setMessageHistory((prev) => prev.concat(""));
+    }
+  }, [lastMessage, setMessageHistory]);
 
   const handleClickSendMessage = useCallback(() => {
     sendMessage(messageInput);
   }, [messageInput, sendMessage]);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
 
   return (
     <>
       <div>
         <div className="chat">
           <div className="chat-title">
-            <h1>Chatroom</h1>
+            <h1>Chatroom ({connectionStatus})</h1>
           </div>
           <div className="messages">
             <div className="messages-content">
-              {[1, 2, 3].map((i) => (
-                <ChatItem key={i}></ChatItem>
+              {messageHistory.map((message, i) => (
+                <ChatItem
+                  userName={`user ${i}`}
+                  timestamp="15:34:15"
+                  text={message}
+                  key={i}
+                ></ChatItem>
               ))}
             </div>
           </div>
@@ -36,6 +57,7 @@ const ChatLayout = () => {
             <button
               type="submit"
               className="message-submit"
+              disabled={readyState !== ReadyState.OPEN}
               onClick={handleClickSendMessage}
             >
               Send
